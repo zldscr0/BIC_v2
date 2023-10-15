4cca46f4a96fcfd3e65b35829d5983ba85309152
Zhixin Bai <201220138@smail.nju.edu.cn>
2023-10-15 20:59:40 +0800

    first

---
+++ core/model/backbone/__init__.py
@@ -0,0 +1,19 @@
+from .resnet import *
+def get_backbone(config):
+    """
+    Get the backbone according to the config dict.
+
+    Args:
+        config: The config dict.
+
+    Returns: The backbone module.
+    """
+
+    kwargs = dict()
+    kwargs.update(config['backbone']['kwargs'])
+    try:
+        emb_func = eval(config["backbone"['name']])(**kwargs)
+    except NameError:
+        raise ("{} is not implemented".format(config["backbone"]['name']))
+    
+    return emb_func
---
+++ core/model/replay/__init__.py
@@ -0,0 +1,6 @@
+from .finetune import Finetune
+from .icarl import ICarl
+from .lucir import LUCIR
+from .lwf import LWF
+from .wa import WA
+from .bic import bic
